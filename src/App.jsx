9c1661a458b0e5d33bf4ada134ab2c95adf80675
Brousse <mbweb47@gmail.com>
2022-01-19 15:34:23 +0100

    ✨Implement routing and create matching pages with functional component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { Fragment } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Error from './pages/Error'
+import Dashboard from './pages/Dashboard'
+import Header from './components/Header'
+
 function App() {
   return (
     <Fragment>
-      <h1>HELLO</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </Fragment>
   )
 }
---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class Error extends React.Component {
+  render() {
+    return <main>404</main>
+  }
+}
+
+export default Error
